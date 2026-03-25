ce19554ddd8c40ae349700da072c49d3379ab61b
asolermaria <asolermaria@gmail.com>
2026-03-25 20:32:43 +0100

    Add funcionalidad botones mapa 1

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //Mapa 1
 var map = L.map("map").setView([20, 0], 2); //Cogemos el elemento "map" del HTML para pintarlo. Con setView, centramos el mapa ([lat, lon], zoom)
-markTerremotos(map, null, [], true); //Llamamos a la función para marcar todos los terremotos en el mapa 1
+let markersMapa1 = []; // Array global para los markers del mapa 1
+markTerremotos(map, null, markersMapa1, true); //Llamamos a la función para marcar todos los terremotos en el mapa 1
 
 //Capa del mapa 1
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -9,104 +10,36 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-//Función para recoger los datos que necesitamos de todos los terremotos de la API
-async function getTerremotos() {
+//Funcionalidad botón mostrar todos los terremotos en el mapa 1
+const botonTodosTerremotos = document.getElementById("boton-todos-terremotos");
+botonTodosTerremotos.addEventListener("click", () => {
+  markTerremotos(map, null, markersMapa1, true);
+});
+
+//Funcionalidad botón mostrar terremotos favoritos en el mapa 1
+const botonFavoritos = document.getElementById("boton-favoritos");
+botonFavoritos.addEventListener("click", async () => {
   try {
-    const data = await fetch(
-      "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson",
-    ).then((res) => res.json());
+    //Eliminamos los markers actuales
+    markersMapa1.forEach((marker) => marker.remove());
 
-    return data.features.map((terremoto) => ({
-      Titulo: terremoto.properties.title,
-      Fecha_inicio: new Date(terremoto.properties.time), //Con new Date pasamos el valor de timestampt (API) a objeto fecha
-      Fecha_fin: new Date(terremoto.properties.updated), //Con new Date pasamos el valor de timestampt (API) a objeto fecha
-      Ubicación: terremoto.properties.place,
-      Código: terremoto.id,
-      Magnitud: terremoto.properties.mag,
-      Coordenadas: [
-        terremoto.geometry.coordinates[1], //A la hora de guardar las coordenadas de la API en nuestro array, invertimos de posición latitud y longitud ya que Leaflet necesita el formato inverso [latitud, longitud]
-        terremoto.geometry.coordinates[0],
-      ],
-    }));
+    //Obtenemos los favoritos de Firestore y guardamos en favoritosArray
+    const favoritosArray = [];
+    const querySnapshot = await db.collection("favoritos").get();
+    querySnapshot.forEach((favorito) => {
+      favoritosArray.push(favorito.data());
+    });
+
+    //Mostramos los favoritos en el mapa
+    markTerremotos(map, favoritosArray, markersMapa1, true);
   } catch (error) {
-    console.log(error);
-    return [];
+    console.error(error);
   }
-}
-
-//Función para marcar los terremotos y añadir popups de los terremotos en el mapa
-async function markTerremotos(
-  mapa,
-  terremotos = null,
-  markersArray = [],
-  esMapa1 = false,
-) {
-  try {
-    if (!terremotos) {
-      //Si no se pasan terremotos, obtenemos todos
-      terremotos = await getTerremotos();
-    }
-
-    //Dependiendo la magnitud de cada terremoto, el marcador tendrá un color u otro
-    for (let terremoto of terremotos) {
-      let colorMarcador;
-      if (terremoto.Magnitud < 1) colorMarcador = "#F0F0F0";
-      else if (terremoto.Magnitud < 2) colorMarcador = "#008000";
-      else if (terremoto.Magnitud < 3) colorMarcador = "#808000";
-      else if (terremoto.Magnitud < 4) colorMarcador = "#FFFF00";
-      else if (terremoto.Magnitud < 5) colorMarcador = "#F0E130";
-      else if (terremoto.Magnitud < 6) colorMarcador = "#FFA500";
-      else if (terremoto.Magnitud < 7) colorMarcador = "#FF0000";
-      else if (terremoto.Magnitud >= 7) colorMarcador = "#FF00FF";
-
-      //Marcador
-      var marker = L.circleMarker(
-        [terremoto.Coordenadas[0], terremoto.Coordenadas[1]],
-        {
-          fillColor: colorMarcador, //Color relleno marcador
-          fillOpacity: 1, //Opacidad relleno marcador
-          color: "#000", //Color borde marcador
-          opacity: 1, //Opacidad borde marcador
-        },
-      ).addTo(mapa);
-
-      //Popup del marcador
-      marker.bindPopup(`
-        <b>${terremoto.Titulo}</b><br>
-        Fecha inicio: ${terremoto.Fecha_inicio.toLocaleString()}<br>
-        Fecha fin: ${terremoto.Fecha_fin.toLocaleString()}<br>
-        Ubicación: ${terremoto.Ubicación}<br>
-        Código: ${terremoto.Código}<br>
-        Magnitud: ${terremoto.Magnitud}<br>
-        ${esMapa1 ? `<button id="boton-favorito">Favorito</button>` : ""} `); //Si es mapa1, creamos el botón favorito en el popup
-
-      //Añadir terremoto a favorito en Firestore
-      marker.on("popupopen", (event) => {
-        //Cuando se abra el popup del marcador del terremoto
-        const botonFavorito = event.popup._contentNode.querySelector("button"); //Guardamos el boton favorito en una variable
-        botonFavorito.addEventListener("click", () => {
-          db.collection("favoritos") //Añadimos a coleccion favoritos de firestore
-            .add({
-              titulo: terremoto.Titulo,
-              fecha_inicio: terremoto.Fecha_inicio.toLocaleString(),
-              fecha_fin: terremoto.Fecha_fin.toLocaleString(),
-              ubicacion: terremoto.Ubicación,
-              codigo: terremoto.Código,
-              magnitud: terremoto.Magnitud,
-            })
-        });
-      });
-
-      //Añadimos cada marcador al array
-      markersArray.push(marker);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
+});
 
 //Mapa 2
 var map2 = L.map("map2").setView([20, 0], 2); //Cogemos el elemento "map" del HTML para pintarlo. Con setView, centramos el mapa ([lat, lon], zoom)
+let markersMapa2 = []; //Array global para los markers del mapa 2
 
 //Capa del mapa 2
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -115,9 +48,9 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map2);
 
-//Filtrado por magnitud
+//Filtrado por magnitud mapa 2
 const inputMagnitud = document.getElementById("input-magnitud");
-let markers = []; //Array donde se van a guardar los marcadores de los terremotos filtrados
+
 inputMagnitud.addEventListener("input", async () => {
   const terremotos = await getTerremotos();
   const inputValor = inputMagnitud.value; //Guardamos el valor introducido en el input del HTML
@@ -128,27 +61,24 @@ inputMagnitud.addEventListener("input", async () => {
     terremotosFiltrados = [];
   } else {
     //Si hay valor introducido en el input, filtramos
-
-    //Convertimos a número el valor introducido en el input
-    const inputNumero = Number(inputValor);
     terremotosFiltrados = terremotos.filter((terremoto) => {
       return (
         //Filtramos el array completo de terremotos y nos quedamos con los que no tengan la propiedad Magnitud vacía y comiencen por el valor introducido en el input (para sacar también los decimales)
-        terremoto.Magnitud != null &&
-        terremoto.Magnitud.toString().startsWith(inputNumero)
+        terremoto.magnitud != null &&
+        terremoto.magnitud.toString().startsWith(inputValor)
       );
     });
   }
 
-  //Cada vez que cambia el input, se borran los marcadores guardados tanto del mapa como del array
-  markers.forEach((marker) => map2.removeLayer(marker));
-  markers = [];
+  //Se borran los marcadores guardados tanto del mapa como del array
+  markersMapa2.forEach((marker) => map2.removeLayer(marker));
+  markersMapa2 = [];
 
   //Llamamos a la función para marcar los terremotos en el mapa
-  markTerremotos(map2, terremotosFiltrados, markers);
+  markTerremotos(map2, terremotosFiltrados, markersMapa2);
 });
 
-//Filtrado por fechas
+//Filtrado por fechas mapa 2
 const inputFechaInicio = document.getElementById("input-fecha-inicio");
 const inputFechaFin = document.getElementById("input-fecha-fin");
 
@@ -180,17 +110,114 @@ async function filterFechas() {
     const terremotos = await getTerremotos();
     terremotosFiltrados = terremotos.filter((terremoto) => {
       return (
-        normalizeDate(new Date(terremoto.Fecha_inicio)) >= fechaInicio &&
-        normalizeDate(new Date(terremoto.Fecha_fin)) <= fechaFin
+        normalizeDate(terremoto.fecha_inicio) >= fechaInicio &&
+        normalizeDate(terremoto.fecha_fin) <= fechaFin
       );
     });
 
-    markers.forEach((marker) => map2.removeLayer(marker));
-    markers = [];
+    markersMapa2.forEach((marker) => map2.removeLayer(marker));
+    markersMapa2 = [];
 
-    markTerremotos(map2, terremotosFiltrados, markers);
+    markTerremotos(map2, terremotosFiltrados, markersMapa2);
   } catch (error) {
-    console.log(error);
+    console.error(error);
+  }
+}
+
+//Función para recoger los datos de los terremotos de la API
+async function getTerremotos() {
+  try {
+    const data = await fetch(
+      "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson",
+    ).then((res) => res.json());
+
+    return data.features.map((terremoto) => ({
+      titulo: terremoto.properties.title,
+      fecha_inicio: new Date(terremoto.properties.time), //Con new Date pasamos el valor de timestampt (API) a objeto fecha
+      fecha_fin: new Date(terremoto.properties.updated), //Con new Date pasamos el valor de timestampt (API) a objeto fecha
+      ubicacion: terremoto.properties.place,
+      codigo: terremoto.id,
+      magnitud: terremoto.properties.mag,
+      coordenadas: [
+        terremoto.geometry.coordinates[1], //A la hora de guardar las coordenadas de la API en nuestro array, invertimos de posición latitud y longitud ya que Leaflet necesita el formato inverso [latitud, longitud]
+        terremoto.geometry.coordinates[0],
+      ],
+    }));
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+//Función para marcar los terremotos y añadir popups de los terremotos en el mapa
+async function markTerremotos(
+  mapa,
+  terremotos = null,
+  markers,
+  esMapa1 = false,
+) {
+  try {
+    if (!terremotos) {
+      //Si no se pasan terremotos, obtenemos todos
+      terremotos = await getTerremotos();
+    }
+
+    //Dependiendo la magnitud de cada terremoto, el marcador tendrá un color u otro
+    for (let terremoto of terremotos) {
+      let colorMarcador;
+      if (terremoto.magnitud < 1) colorMarcador = "#F0F0F0";
+      else if (terremoto.magnitud < 2) colorMarcador = "#008000";
+      else if (terremoto.magnitud < 3) colorMarcador = "#808000";
+      else if (terremoto.magnitud < 4) colorMarcador = "#FFFF00";
+      else if (terremoto.magnitud < 5) colorMarcador = "#F0E130";
+      else if (terremoto.magnitud < 6) colorMarcador = "#FFA500";
+      else if (terremoto.magnitud < 7) colorMarcador = "#FF0000";
+      else if (terremoto.magnitud >= 7) colorMarcador = "#FF00FF";
+
+      //Marcador
+      var marker = L.circleMarker(
+        [terremoto.coordenadas[0], terremoto.coordenadas[1]],
+        {
+          fillColor: colorMarcador, //Color relleno marcador
+          fillOpacity: 1, //Opacidad relleno marcador
+          color: "#000", //Color borde marcador
+          opacity: 1, //Opacidad borde marcador
+        },
+      ).addTo(mapa);
+
+      //Popup del marcador
+      marker.bindPopup(`
+        <b>${terremoto.titulo}</b><br>
+        Fecha inicio: ${terremoto.fecha_inicio.toLocaleString()}<br>
+        Fecha fin: ${terremoto.fecha_fin.toLocaleString()}<br>
+        Ubicación: ${terremoto.ubicacion}<br>
+        Código: ${terremoto.codigo}<br>
+        Magnitud: ${terremoto.magnitud}<br>
+        ${esMapa1 ? `<button>Favorito</button>` : ""} `); //Si es mapa1, creamos el botón favorito en el popup
+
+      //Funcionalidad boton añadir favorito a Firestore
+      marker.on("popupopen", (event) => {
+        //Cuando se abra el popup del marcador del terremoto
+        const botonFavorito = event.popup._contentNode.querySelector("button"); //Guardamos el boton favorito en una variable
+        botonFavorito.addEventListener("click", () => {
+          db.collection("favoritos") //Añadimos a coleccion favoritos de firestore
+            .add({
+              titulo: terremoto.titulo,
+              fecha_inicio: terremoto.fecha_inicio.toLocaleString(),
+              fecha_fin: terremoto.fecha_fin.toLocaleString(),
+              ubicacion: terremoto.ubicacion,
+              codigo: terremoto.codigo,
+              magnitud: terremoto.magnitud,
+              coordenadas: terremoto.coordenadas,
+            });
+        });
+      });
+
+      //Añadimos cada marcador al array
+      markers.push(marker);
+    }
+  } catch (error) {
+    console.error(error);
   }
 }
 
@@ -202,6 +229,7 @@ function normalizeDate(date) {
 }
 
 //FIREBASE
+
 //Configuración proyecto firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCPkI7rlte3aSt-xv9e-mXSbpuS72CQqps",
@@ -211,9 +239,13 @@ const firebaseConfig = {
   messagingSenderId: "656011203905",
   appId: "1:656011203905:web:0315fc3d8b18ed6edc7dc0",
 };
+
 // Inicializar Firebase
 const app = firebase.initializeApp(firebaseConfig);
+
 // Inicializar Firestore
 const db = firebase.firestore();
+
 // Inicializar Auth
 const auth = firebase.auth();
+
